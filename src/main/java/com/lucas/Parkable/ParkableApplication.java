642cd3715676c8359b0ce84b3a1cd17ecf80a3f3
Lucas <lucasbo.dev@gmail.com>
2025-09-03 14:54:18 -0300

    chore: initialize project structure

---
+++ src/main/java/com/lucas/Parkable/ParkableApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.Parkable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/Parkable/ParkableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.Parkable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
