aeec3270e7ed9afb725ffa47fb413c6e548e1042
Lucas <lucasbo.dev@gmail.com>
2025-09-08 20:47:38 -0300

    feat: add query methods to find vagas by occupation status

--- src/main/java/com/lucas/Parkable/Repository/VagaRepository.java
+++ src/main/java/com/lucas/Parkable/Repository/VagaRepository.java
@@ -2,7 +2,11 @@ package com.lucas.Parkable.Repository;
 
 import com.lucas.Parkable.Models.VagaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface VagaRepository extends JpaRepository <VagaModel, Long> {
 
+    List<VagaModel> findByOcupadaIsTrue();
+
+    List<VagaModel> findByOcupadaIsFalse();
 }
