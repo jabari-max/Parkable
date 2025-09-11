da789b9897b643a7553b6245398bc53a943ca2b4
Lucas <lucasbo.dev@gmail.com>
2025-09-11 11:19:03 -0300

    feat: implement methods for queries specifics

--- src/main/java/com/lucas/Parkable/Repository/RegistroEstacionamentoRepository.java
+++ src/main/java/com/lucas/Parkable/Repository/RegistroEstacionamentoRepository.java
@@ -1,8 +1,13 @@
 package com.lucas.Parkable.Repository;
 
+import com.lucas.Parkable.Enums.TipoVeiculo;
 import com.lucas.Parkable.Models.RegistroEstacionamentoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface RegistroEstacionamentoRepository extends JpaRepository <RegistroEstacionamentoModel, Long> {
 
+    List<RegistroEstacionamentoModel> findByPlaca(String placa);
+
+    List<RegistroEstacionamentoModel> findByTipoVeiculo(TipoVeiculo tipoVeiculo);
 }
