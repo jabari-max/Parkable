54895484752078374603074a2812b75d955f94e3
Lucas <lucasbo.dev@gmail.com>
2025-09-11 12:44:52 -0300

    feat: implement API endpoints for RegistroEstacionamentoController

--- src/main/java/com/lucas/Parkable/Controllers/RegistroEstacionamentoController.java
+++ src/main/java/com/lucas/Parkable/Controllers/RegistroEstacionamentoController.java
@@ -1,22 +1,48 @@
 package com.lucas.Parkable.Controllers;
 
+import com.lucas.Parkable.Enums.TipoVeiculo;
+import com.lucas.Parkable.Models.RegistroEstacionamentoModel;
 import com.lucas.Parkable.Service.RegistroEstacionamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/estacionamento")
 public class RegistroEstacionamentoController {
 
     @Autowired
     private RegistroEstacionamentoService registroEstacionamentoService;
 
-    /*TODO:
-    *  - Adicionar um carro
-    *  - Alterar um carro
-    *  - Exibir carros por placa
-    *  - Registrar hora de saída (aplicar regra de negócio do preço a pagar)
-    *  - Listar apenas carros
-    *  - Listar apenas motos*/
+    @PostMapping("/registrarEntrada/{placa}/{vagaId}/{tipoVeiculo}")
+    public RegistroEstacionamentoModel registarEntrada(@PathVariable String placa, @PathVariable Long vagaId, @PathVariable TipoVeiculo tipoVeiculo){
+        return registroEstacionamentoService.registrarEntrada(placa, vagaId, tipoVeiculo);
+    }
+
+    @PutMapping("/registrarSaida/{id}")
+    public RegistroEstacionamentoModel registarSaida (@PathVariable Long id){
+        return registroEstacionamentoService.registrarSaida(id);
+    }
+
+    @GetMapping("/listar")
+    public List<RegistroEstacionamentoModel> exibirRegistros(){
+        return registroEstacionamentoService.exibirRegistros();
+    }
+
+    @GetMapping("/listar/tipo/{tipoVeiculo}")
+    public List<RegistroEstacionamentoModel> exibirRegistrosTipo(@PathVariable TipoVeiculo tipoVeiculo){
+        return registroEstacionamentoService.exibirRegistrosTipo(tipoVeiculo);
+    }
+
+    @GetMapping("/listar/placa/{placa}")
+    public List<RegistroEstacionamentoModel> exibirRegistrosPlaca(@PathVariable String placa){
+        return registroEstacionamentoService.exibirRegistrosPlaca(placa);
+    }
+
+    @GetMapping("/listarPresentes")
+    public List<RegistroEstacionamentoModel> exibirRegistrosPresentes(){
+        return registroEstacionamentoService.exibirRegistrosPresentes();
+    }
+
 }
